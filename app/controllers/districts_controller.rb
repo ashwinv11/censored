01a7b6de8b04540a8f59242736b38917bcb2c5d3
ashwinv11 <ashwinv11@gmail.com>
2016-04-11 12:45:26 -0700

    added railways shapefile

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -2,11 +2,11 @@ class DistrictsController < ApplicationController
   def index
     @districts = []
     client = Mongo::Client.new(['127.0.0.1:27017'], :database => 'censored_development')
-    
+  
     cursor = client[:district].find(:DISTRICT => 'PUDUCHERRY').each do |document|
       @districts.push(document)
     end
-
+    
     render :json => @districts
   end
 end

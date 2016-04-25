04a217b5a965c32c58161489732f954727c42c54
ashwinv11 <ashwinv11@gmail.com>
2016-04-25 02:10:52 -0700

    fuckin reset button

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,4 +1,4 @@
-var w = 1000;
+var w = 800;
 var h = 800;
 var proj = d3.geo.mercator();
 var path = d3.geo.path().projection(proj);
@@ -6,6 +6,7 @@ var t = proj.translate(); // the projection's default translation
 var s = proj.scale() // the projection's default scale
 var circles = [];
 var centered;
+var linePath;
 
 var map = d3.select("#chart").append("svg:svg")
     .attr("width", w)
@@ -19,15 +20,13 @@ var india = map.append("svg:g")
 
 function initialize() {
   proj.scale(7200);
-  proj.translate([-950, 780]);
+  proj.translate([-1280, 780]);
 }
 
 var layer2 = map.append('g')
             .attr("id", "lines");
   
-var lines = layer2.append("svg")
-    .attr("width", w)
-    .attr("height", h);
+var lines = layer2.append("svg");
 
 function drawMap() {    
   d3.json('/india_district.geojson', function (json) {
@@ -39,6 +38,7 @@ function drawMap() {
         .attr("opacity", .6)
         .attr("stroke", "black")
         .attr("stroke-width", .4)
+        .attr("stroke-opacity", .4)
         .on("click", function(d) {
           console.log(d.properties.NAME_2);
 
@@ -56,26 +56,24 @@ function drawMap() {
           var mousePos = d3.mouse(this);
           circles.push(mousePos);
 
-          lines.append("path")
-                .attr("d", lineFunction(circles))
-                .attr("stroke-width", 2)
-                .attr("stroke", "white")
-                .style("fill", "none")
-                .attr("opacity", ".4")
-                .attr("stroke-dashoffset", this.getTotalLength())
-                .transition()
-                .duration(2000)
-                .ease("linear")
-                .attr("stroke-dashoffset", 0);          
+          linePath = lines.append("path")
+                    .attr("class", "line")
+                    .attr("d", lineFunction(circles))
+                    .attr("stroke-width", 2)
+                    .attr("stroke", "white")
+                    .style("fill", "none")
+                    .attr("opacity", ".4")
+                    .style("stroke-dasharray", ("8,4"));          
 
-          lines.selectAll("circle")
-                .data(circles)
-                .enter()
-                .append("circle")
-                .attr("cx",function(d,i){return circles[i][0];})
-                .attr("cy",function(d,i){return circles[i][1];})
-                .attr("r",2)
-                .style("fill", "white");
+          linePath = lines.selectAll("circle")
+                    .data(circles)
+                    .enter()
+                    .append("circle")
+                    .attr("cx",function(d,i){return circles[i][0];})
+                    .attr("cy",function(d,i){return circles[i][1];})
+                    .attr("r",2)
+                    .attr("class", "line")
+                    .style("fill", "white");
 
           var x, y, k;
 
@@ -109,4 +107,9 @@ function drawMap() {
               .style("stroke-width", 1.5 / k + "px");
           })
   });
+}
+
+function reset() {
+  d3.selectAll(".line").remove();
+  circles = [];
 }
